e81ef0da1e67d711f82778c3cda45c3a3c4b2ec2
Marchalek <marchalek@hotmail.com>
2021-07-01 12:15:20 -0300

    finalizando ultima aula

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -1,16 +1,30 @@
+import React, { Component } from "react";
 import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
-import { Component } from "react";
+import "./assets/App.css";
+import './assets/index.css';
+class App extends Component {
 
-class App extends Component() {
+  constructor(){
+    super()
+    this.notas = []
+    this.state
+  }
+
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto}
+    this.notas.push(novaNota)
+    console.log(this.notas.length)
+  }
   render() {
     return (
-      <section>
-        <FormularioCadastro />
-        <ListaDeNotas />
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas notas={this.notas}/>
       </section>
     );
   }
 }
+
 
 export default App;
